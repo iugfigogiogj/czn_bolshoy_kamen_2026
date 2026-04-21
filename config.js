aad7b4218ffefc35198d0d0e6953ea2370ca8561
CZN <maximvdovenko2007@gmail.com>
2026-04-21 17:46:05 +1000

    Update config.js

--- config.js
+++ config.js
@@ -1,16 +1,13 @@
 // ========== КОНФИГУРАЦИЯ САЙТА ==========
 
 const CONFIG = {
-    // URL сервера
-    API_URL: 'https://CZN.pythonanywhere.com',
-    
     // Настройки сайта
     SITE_NAME: 'ЦЗН г. Большой Камень',
     SITE_URL: window.location.origin,
-    
+
     // Пароль для админки
     ADMIN_PASSWORD: 'B0lsh0jK@men',
-    
+
     // Ключи для localStorage (только для авторизации)
     STORAGE_KEYS: {
         AUTH: 'czbn_auth'
